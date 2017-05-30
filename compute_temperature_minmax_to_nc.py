432d971f0fbdfb2e7ff780b2de41aae86873ef3b
EarthScientist <lindgren.mike@gmail.com>
2017-05-30 06:27:28 -0800

    rearrange and cleanup

--- compute_temperature_minmax_to_nc.py
+++ compute_temperature_minmax_to_nc.py
@@ -11,18 +11,25 @@ variable = 't2'
 # read in mfdataset
 ds = xr.open_mfdataset( os.path.join( input_path, variable, '*'.join(['', variable.upper(), '.nc']) ) )
 
-# convert to Daily Averages
-output_path = os.path.join( input_path.replace( 'hourly', 'daily' ) )
+# set up some output pathing for the new aggregates
+output_path_day = os.path.join( input_path.replace( 'hourly', 'daily' ) )
+if not os.path.exists( output_path_day ):
+	os.makedirs( output_path_day )
+
+output_path_mon = os.path.join( input_path.replace( 'hourly', 'monthly' ) )
+if not os.path.exists( output_path_mon ):
+	os.makedirs( output_path_mon )
 
 # temp min -- daily
 ds_min = ds.resample( 'D', dim='time', how='min' )
 ds_min_comp = ds_min.compute()
-ds_min.to_netcdf( os.path.join( output_path, 'T2_min_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+ds_min.to_netcdf( os.path.join( output_path_day, 'T2_min_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+
 
 # temp min -- monthly
 ds_min_mon = ds_min_comp.resample( 'M', dim='time', how='mean' )
 ds_min_mon_comp = ds_min_mon.compute()
-ds_min_mon_comp.to_netcdf( os.path.join( output_path, 'T2_max_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+ds_min_mon_comp.to_netcdf( os.path.join( output_path_mon, 'T2_max_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
 
 # cleanup...
 ds_min.close(); 
@@ -34,12 +41,12 @@ ds_min_mon_comp = None
 # temp max -- daily
 ds_max = ds.resample( 'D', dim='time', how='max' )
 ds_max_comp = ds_min.compute()
-ds_max.to_netcdf( os.path.join( output_path, 'T2_max_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+ds_max.to_netcdf( os.path.join( output_path_day, 'T2_max_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
 
 # temp max -- monthly
 ds_max_mon = ds_max_comp.resample( 'M', dim='time', how='mean' )
 ds_max_mon_comp = ds_max_mon.compute()
-ds_min_mon_comp.to_netcdf( os.path.join( output_path, 'T2_max_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+ds_max_mon_comp.to_netcdf( os.path.join( output_path_mon, 'T2_max_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
 
 # cleanup...
 ds_max.close(); 
