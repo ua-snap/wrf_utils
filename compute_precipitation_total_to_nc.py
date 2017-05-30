f30e6e98490c0b6a2105f2f43b68529b37796138
EarthScientist <lindgren.mike@gmail.com>
2017-05-30 08:31:45 -0800

    update tmax/tmin variable naming and add attrs to temperature and precipitation output NC files

--- compute_precipitation_total_to_nc.py
+++ compute_precipitation_total_to_nc.py
@@ -11,6 +11,12 @@ variable = 'pcpt'
 # read in mfdataset
 ds = xr.open_mfdataset( os.path.join( input_path, variable, '*'.join(['', variable.upper(), '.nc']) ) )
 
+# get the attrs from the input dset
+global_attrs = ds.attrs
+local_attrs = ds[ variable.upper() ].attrs
+xy_attrs = ds[ 'lon' ].attrs
+# time_attrs = ds[ 'time' ].attrs
+
 # set up some output pathing for the new aggregates
 # day
 output_path_day = os.path.join( input_path.replace( 'hourly', 'daily' ) )
@@ -23,11 +29,39 @@ if not os.path.exists( output_path_mon ):
 
 # precip total -- daily
 ds_total = ds.resample( 'D', dim='time', how='sum' )
-ds_total_comp = ds_min.compute()
-ds_total_comp.to_netcdf( os.path.join( output_path_day, 'PCPT_total_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+ds_total_comp = ds_total.compute()
+
+# setup the output dataset with proper varname and attrs
+# ds_total_comp[ variable.upper() ].to_dataset( name=variable.upper() )
+ds_total_comp.attrs = global_attrs
+ds_total_comp[ variable.upper() ].attrs = local_attrs
+ds_total_comp[ 'lon' ].attrs = xy_attrs
+ds_total_comp[ 'lat' ].attrs = xy_attrs
+
+ds_total_comp.to_netcdf( os.path.join( output_path_day, '{}_total_wrf_day.nc'.format(variable) ), mode='w', format='NETCDF4_CLASSIC' )
 
 # precip total -- monthly
 ds_total_mon = ds_total_comp.resample( 'M', dim='time', how='sum' )
 ds_total_mon_comp = ds_total_mon.compute()
-ds_total_comp.to_netcdf( os.path.join( output_path_mon, 'PCPT_total_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
 
+# setup the output dataset with proper varname and attrs
+# ds_total_mon_comp[ variable.upper() ].to_dataset( name=variable.upper() )
+ds_total_mon_comp.attrs = global_attrs
+ds_total_mon_comp[ variable.upper() ].attrs = local_attrs
+ds_total_mon_comp[ 'lon' ].attrs = xy_attrs
+ds_total_mon_comp[ 'lat' ].attrs = xy_attrs
+
+ds_total_mon_comp.to_netcdf( os.path.join( output_path_mon, '{}_total_wrf_month.nc'.format(variable) ), mode='w', format='NETCDF4_CLASSIC' )
+
+# cleanup...
+ds_total.close()
+ds_total = None
+ds_total_comp.close()
+ds_total_comp = None
+ds_total_mon.close()
+ds_total_mon = None
+ds_total_mon_comp.close()
+ds_total_mon_comp = None
+
+# close master
+ds.close()
--- compute_temperature_minmax_to_nc.py
+++ compute_temperature_minmax_to_nc.py
@@ -11,6 +11,12 @@ variable = 't2'
 # read in mfdataset
 ds = xr.open_mfdataset( os.path.join( input_path, variable, '*'.join(['', variable.upper(), '.nc']) ) )
 
+# get the attrs from the input dset
+global_attrs = ds.attrs
+local_attrs = ds[ variable.upper() ].attrs
+xy_attrs = ds[ 'lon' ].attrs
+# time_attrs = ds[ 'time' ].attrs
+
 # set up some output pathing for the new aggregates
 # day
 output_path_day = os.path.join( input_path.replace( 'hourly', 'daily' ) )
@@ -24,38 +30,72 @@ if not os.path.exists( output_path_mon ):
 # temp min -- daily
 ds_min = ds.resample( 'D', dim='time', how='min' )
 ds_min_comp = ds_min.compute()
-ds_min_comp.to_netcdf( os.path.join( output_path_day, 'T2_min_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+
+# setup the output dataset with proper varname and attrs
+new_varname = 'TMIN'
+ds_min_comp[ variable.upper ].to_dataset( name=new_varname )
+ds_min_comp.attrs = global_attrs
+ds_min_comp[ new_varname ].attrs = local_attrs
+ds_min_comp[ 'lon' ].attrs = xy_attrs
+ds_min_comp[ 'lat' ].attrs = xy_attrs
+
+ds_min_comp.to_netcdf( os.path.join( output_path_day, '{}_wrf_day.nc'.format( new_varname.lower() ) ), mode='w', format='NETCDF4_CLASSIC' )
 
 # temp min -- monthly
 ds_min_mon = ds_min_comp.resample( 'M', dim='time', how='mean' )
 ds_min_mon_comp = ds_min_mon.compute()
-ds_min_mon_comp.to_netcdf( os.path.join( output_path_mon, 'T2_max_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+
+# setup the output dataset with proper varname and attrs
+# ds_min_comp[ variable.upper ].to_dataset( name=new_varname )
+ds_min_mon_comp.attrs = global_attrs
+ds_min_mon_comp[ new_varname ].attrs = local_attrs
+ds_min_mon_comp[ 'lon' ].attrs = xy_attrs
+ds_min_mon_comp[ 'lat' ].attrs = xy_attrs
+
+ds_min_mon_comp.to_netcdf( os.path.join( output_path_mon, '{}_wrf_month.nc'.format( new_varname.lower() ) ), mode='w', format='NETCDF4_CLASSIC' )
 
 # cleanup...
-ds_min.close(); 
+ds_min.close()
 ds_min = None
-ds_min_comp.close(); 
+ds_min_comp.close()
 ds_min_comp = None
+ds_min_mon_comp.close()
 ds_min_mon_comp = None
 
 # temp max -- daily
 ds_max = ds.resample( 'D', dim='time', how='max' )
-ds_max_comp = ds_min.compute()
-ds_max_comp.to_netcdf( os.path.join( output_path_day, 'T2_max_wrf_day.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+ds_max_comp = ds_max.compute()
+
+# setup the output dataset with proper varname and attrs
+new_varname = 'TMAX'
+ds_max_comp[ variable.upper ].to_dataset( name=new_varname )
+ds_max_comp.attrs = global_attrs
+ds_max_comp[ new_varname ].attrs = local_attrs
+ds_max_comp[ 'lon' ].attrs = xy_attrs
+ds_max_comp[ 'lat' ].attrs = xy_attrs
+
+ds_max_comp.to_netcdf( os.path.join( output_path_day, '{}_wrf_day.nc'.format( new_varname.lower() ) ), mode='w', format='NETCDF4_CLASSIC' )
 
 # temp max -- monthly
 ds_max_mon = ds_max_comp.resample( 'M', dim='time', how='mean' )
 ds_max_mon_comp = ds_max_mon.compute()
-ds_max_mon_comp.to_netcdf( os.path.join( output_path_mon, 'T2_max_wrf_month.nc' ), mode='w', format='NETCDF4_CLASSIC' )
+
+# setup the output dataset with proper varname and attrs
+# ds_min_comp[ variable.upper ].to_dataset( name=new_varname )
+ds_max_mon_comp.attrs = global_attrs
+ds_max_mon_comp[ new_varname ].attrs = local_attrs
+ds_max_mon_comp[ 'lon' ].attrs = xy_attrs
+ds_max_mon_comp[ 'lat' ].attrs = xy_attrs
+
+ds_max_mon_comp.to_netcdf( os.path.join( output_path_mon, '{}_wrf_month.nc'.format( new_varname.lower() ) ), mode='w', format='NETCDF4_CLASSIC' )
 
 # cleanup...
-ds_max.close(); 
+ds_max.close()
 ds_max = None
-ds_max_comp.close(); 
+ds_max_comp.close()
 ds_max_comp = None
-ds_max_mon_comp.close(); 
+ds_max_mon_comp.close()
 ds_max_mon_comp = None
 
 # close master
 ds.close()
-
