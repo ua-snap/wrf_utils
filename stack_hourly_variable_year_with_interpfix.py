919f8770cf4bc38cf79cba0b567515d13a84fd41
EarthScientist <lindgren.mike@gmail.com>
2017-06-07 21:25:22 -0800

    CLI

--- stack_hourly_variable_year_with_interpfix.py
+++ stack_hourly_variable_year_with_interpfix.py
@@ -1,3 +1,5 @@
+#! v3/bin/python3
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #
 # stack the hourly data and diff/interpolate the accumulation variables
@@ -99,7 +101,7 @@ def run_year( df, variable ):
     # do special stuff to the accumulation vars
     if variable in DIFF_VARS:
         print( 'interpolating missing hours --> {}'.format( variable ) )
-        # diff it (T - (T-1)) and add back that lost timestep -- with a copy?
+        # diff it (T - (T-1)) and add back that lost timeste
         diff_arr = np.diff( arr[::-1,...], axis=0 )[::-1,...] # double flip!
         # add the missing file back? to the beginning.
         # # if we need to reverse the series pre-diff we should do that first...
@@ -129,21 +131,37 @@ if __name__ == '__main__':
     import xarray as xr
     import numpy as np
     import pandas as pd
+    import argparse
 
-    # list files
-    input_path = '/storage01/pbieniek/gfdl/hist/hourly'
-    group = 'gfdl_hist'
-    variable = 'PCPT'
-    files_df_fn = '/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/docs/WRFDS_forecast_time_attr_{}.csv'.format( group )
-    output_path = '/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/v2'
-    monthly_template_fn = '/storage01/pbieniek/gfdl/hist/monthly/monthly_{}-gfdlh.nc'.format( variable )
+    # parse some args
+    parser = argparse.ArgumentParser( description='stack the hourly outputs from raw WRF outputs to NetCDF files of hourlies broken up by year.' )
+    parser.add_argument( "-i", "--input_path", action='store', dest='input_path', type=str, help="input hourly directory with annual sub-dirs containing raw WRF NetCDF outputs" )
+    parser.add_argument( "-y", "--year", action='store', dest='year', type=int, help="year to process" )
+    parser.add_argument( "-f", "--files_df_fn", action='store', dest='files_df_fn', type=str, help="path to the .csv file containing parsed filename and forecast_time already precomputed " )
+    parser.add_argument( "-v", "--variable", action='store', dest='variable', type=str, help="variable name (exact)" )
+    parser.add_argument( "-o", "--output_path", action='store', dest='output_filename', type=str, help="output filename for the new NetCDF hourly data for the input year" )
+    parser.add_argument( "-t", "--template_fn", action='store', dest='template_fn', type=str, help="monthly template file that is used for passing global metadata to output NC files." )
+    
+    # parse the args and unpack
+    args = parser.parse_args()
+    input_path = args.input_path
+    year = args.year
+    files_df_fn = args.files_df_fn
+    variable = args.variable
+    output_path = args.output_path
+    template_fn = args.template_fn
 
-    # wrf output standard vars
+    # # FOR TESTING
+    # input_path = '/storage01/pbieniek/gfdl/hist/hourly'
+    # group = 'gfdl_hist'
+    # variable = 'PCPT'
+    # files_df_fn = '/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/docs/WRFDS_forecast_time_attr_{}.csv'.format( group )
+    # output_path = '/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/v2'
+    # template_fn = '/storage01/pbieniek/gfdl/hist/monthly/monthly_{}-gfdlh.nc'.format( variable )
+
+    # wrf output standard vars -- [hardwired] for now
     lon_variable = 'g5_lon_1'
     lat_variable = 'g5_lat_0'
-
-    # # list files to a df
-    # df = list_files( input_path )
 
     # read in pre-built dataframe with forecast_time as a field
     df = pd.read_csv( files_df_fn, sep=',', index_col=0 )
@@ -160,15 +178,12 @@ if __name__ == '__main__':
     year = 1990
     sub_df = df[ (df.year == year) & (df.folder_year == year) ].reset_index()
     arr = run_year( sub_df, variable )
-    
-    # RUNNING ABOVE
-
 
     # build the output NetCDF Dataset
     new_dates = pd.date_range( '-'.join(sub_df.loc[sub_df.index[0],['month','day','year']].astype(str)), periods=arr.shape[0], freq='1H' )
 
     # get some template data to get some vars from... -- HARDWIRED...
-    mon_tmp_ds = xr.open_dataset( monthly_template_fn, decode_times=False )
+    mon_tmp_ds = xr.open_dataset( template_fn, decode_times=False )
     tmp_ds = xr.open_dataset( sub_df.fn.tolist()[0] )
     global_attrs = tmp_ds.attrs.copy()
     global_attrs[ 'reference_time' ] = str(new_dates[0]) # 1979 hourly does NOT start at day 01...  rather day 02....
@@ -200,3 +215,19 @@ if __name__ == '__main__':
 
     output_filename = os.path.join( final_path, '{}_wrf_hourly_{}_{}.nc'.format(variable, group, year) )
     ds.to_netcdf( output_filename, mode='w', format='NETCDF4_CLASSIC' )
+
+
+# # # # # EXAMPLE # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # FOR TESTING
+# import os, subprocess
+
+# input_path = '/storage01/pbieniek/gfdl/hist/hourly'
+# group = 'gfdl_hist'
+# variable = 'PCPT'
+# files_df_fn = '/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/docs/WRFDS_forecast_time_attr_{}.csv'.format( group )
+# output_path = '/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/v2'
+# template_fn = '/storage01/pbieniek/gfdl/hist/monthly/monthly_{}-gfdlh.nc'.format( variable )
+
+# os.chdir( '/workspace/UA/malindgren/repos/wrf_utils' )
+# _ = subprocess.call(['stack_hourly_variable_year_with_interpfix.py', '--', '-i', 'input_path', '-y', 'group', '-f', 'variable', '-v', 'files_df_fn', '-o', 'output_path', '-t', 'template_fn'])
+
