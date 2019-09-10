107e5c6fdece4356b8d0f839f7735ed1e5e9520c
EarthScientist <lindgren.mike@gmail.com>
2019-09-09 16:58:44 -0800

    big updates for full run

--- snap_wrf_data_prep/make_bash_slurm/make_variable_sbatch_by_year.py
+++ snap_wrf_data_prep/make_bash_slurm/make_variable_sbatch_by_year.py
@@ -1,4 +1,4 @@
-def write_batch(fn, variable, output_filename, year):
+def write_batch(fn, variable, output_filename, year, input_path, dione_path):
     head = \
     "#!/bin/sh\n"+\
     "#SBATCH --nodes=1\n"+\
@@ -9,8 +9,8 @@ def write_batch(fn, variable, output_filename, year):
     "#SBATCH -p main\n\n"
 
     args = "SCRIPTNAME=/workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/stack_hourly_variable_year.py\n"+\
-            "DIONEPATH=/storage01/rtladerjr/hourly\n"+\
-            "INPATH=/atlas_scratch/malindgren/WRF_DATA\n"+\
+            "DIONEPATH={}\n".format(dione_path)+\
+            "INPATH={}\n".format(input_path)+\
             "FILES_DF_FN=/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/docs/WRFDS_forecast_time_attr_gfdl_rcp85.csv\n" +\
             "VARIABLE={}\n".format(variable) +\
             "OUTPUT_FILENAME={}\n".format(output_filename) +\
@@ -22,7 +22,7 @@ def write_batch(fn, variable, output_filename, year):
         f.write( head + '\n' + args + '\n' )
     return fn
 
-def write_batch_winds(fn, variable, output_filename, year):
+def write_batch_winds(fn, variable, output_filename, year, input_path, dione_path):
     head = \
     "#!/bin/sh\n"+\
     "#SBATCH --nodes=1\n"+\
@@ -33,8 +33,8 @@ def write_batch_winds(fn, variable, output_filename, year):
     "#SBATCH -p main\n\n"
 
     args = "SCRIPTNAME=/workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/stack_hourly_variable_year_winds.py\n"+\
-            "DIONEPATH=/storage01/rtladerjr/hourly\n"+\
-            "INPATH=/atlas_scratch/malindgren/WRF_DATA\n"+\
+            "DIONEPATH=\n".format(dione_path)+\
+            "INPATH={}\n".format(input_path)+\
             "FILES_DF_FN='/workspace/Shared/Tech_Projects/wrf_data/project_data/wrf/docs/WRFDS_forecast_time_attr_gfdl_rcp85.csv\n" +\
             "VARIABLE={}\n".format(variable) +\
             "OUTPUT_FILENAME={}\n".format(output_filename) +\
@@ -52,27 +52,40 @@ if __name__ == '__main__':
     import itertools, os
 
     # some set up args
-    group = 'gfdl_rcp85'
-    variable = 'PCPT'
-    output_path = '/rcs/project_data/WRF_DATA_SEP2019'
-    years = range(2006, 2100+1)
-    variables = ['ACSNOW', 'T2', 'CANWAT', 'CLDFRA', 'HFX', 'LH', 'LWDNB', 'LWUPB', 'PCPC', 'PCPNC', 'PCPT', 'POTEVP', 
-            'QBOT', 'Q2', 'SNOW', 'QVAPOR', 'SNOWC', 'SNOWH', 'SWUPB', 'SWDNB', 'TSLB', 'ALBEDO', 'VEGFRA', 
-            'CLDFRA_HIGH', 'CLDFRA_LOW', 'CLDFRA_MID', 'LWUPBC', 'LWDNBC', 'GHT', 'OMEGA', 'PSFC', 'SLP', 'SH2O', 
-            'SEAICE', 'SWUPBC', 'SMOIS', 'SWDNBC', 'TBOT', 'TSK', 'T', 'U', 'V', 'U10', 'V10', 'UBOT', 'VBOT']
+    groups = ['gfdl_rcp85','ccsm_hist', 'erain', 'ccsm_rcp85', 'gfdl_hist']
+    for group in groups:
+        dione_paths = {'gfdl_rcp85':'/storage01/rtladerjr/hourly', 
+                    'ccsm_rcp85':'/storage01/pbieniek/ccsm/rcp85/hourly', 
+                    'gfdl_hist':'/storage01/pbieniek/gfdl/hist/hourly', 
+                    'ccsm_hist':'/storage01/pbieniek/ccsm/hist/hourly', 
+                    'erain':'/storage01/pbieniek/erain/hourly'}
 
-    # write the batch file
-    for year, variable in itertools.product(years, variables):
-        output_filename = os.path.join(output_path,variable.lower(),'{}_wrf_hourly_{}_{}.nc'.format(variable, group, year))
-        fn = '/workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/make_bash_slurm/slurm_scripts/{1}/{0}_{1}_{2}.slurm'.format(variable,year,group)
-        print('writing:{}'.format(os.path.basename(fn)))
-        # make sure there are dirs to dump the files
-        dirname = os.path.dirname(fn)
-        if not os.path.exists(dirname):
-            _ = os.makedirs(dirname)
+        year_ranges = {'gfdl_rcp85':(2006,2100), 'ccsm_rcp85':(2005,2100),
+                    'gfdl_hist':(1970,2006), 'ccsm_hist':(1970,2005), 'erain':(1979,2018)}
 
-        if variable in ['U','V','U10','V10','UBOT','VBOT']:
-            _ = write_batch_winds(fn, variable, output_filename, year)
-        else:
-            _ = write_batch(fn, variable, output_filename, year)
+        begin,end = year_ranges[group]
+
+        dione_path=dione_paths[group]
+        input_path='/atlas_scratch/malindgren/WRF_DATA/{}'.format(group)
+        output_path = '/rcs/project_data/WRF_DATA_SEP2019'
+        years = range(begin, end+1)
+        variables = ['ACSNOW', 'T2', 'CANWAT', 'CLDFRA', 'HFX', 'LH', 'LWDNB', 'LWUPB', 'PCPC', 'PCPNC', 'PCPT', 'POTEVP', 
+                'QBOT', 'Q2', 'SNOW', 'QVAPOR', 'SNOWC', 'SNOWH', 'SWUPB', 'SWDNB', 'TSLB', 'ALBEDO', 'VEGFRA', 
+                'CLDFRA_HIGH', 'CLDFRA_LOW', 'CLDFRA_MID', 'LWUPBC', 'LWDNBC', 'GHT', 'OMEGA', 'PSFC', 'SLP', 'SH2O', 
+                'SEAICE', 'SWUPBC', 'SMOIS', 'SWDNBC', 'TBOT', 'TSK', 'T', 'U', 'V', 'U10', 'V10', 'UBOT', 'VBOT']
+
+        # write the batch file
+        for year, variable in itertools.product(years, variables):
+            output_filename = os.path.join(output_path,variable.lower(),'{}_wrf_hourly_{}_{}.nc'.format(variable, group, year))
+            fn = '/workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/make_bash_slurm/slurm_scripts/{1}/{0}_{1}_{2}.slurm'.format(variable,year,group)
+            print('writing:{}'.format(os.path.basename(fn)))
+            # make sure there are dirs to dump the files
+            dirname = os.path.dirname(fn)
+            if not os.path.exists(dirname):
+                _ = os.makedirs(dirname)
+
+            if variable in ['U','V','U10','V10','UBOT','VBOT']:
+                _ = write_batch_winds(fn, variable, output_filename, year, input_path, dione_path)
+            else:
+                _ = write_batch(fn, variable, output_filename, year, input_path, dione_path)
 
--- snap_wrf_data_prep/make_bash_slurm/run_2006_allvars_dependency_full.sh
+++ snap_wrf_data_prep/make_bash_slurm/run_2006_allvars_dependency_full.sh
@@ -2,15 +2,15 @@
 
 # make sure the first year is already moved over before we loop
 FIRSTYEAR=2006
+GROUPNAME=gfdl_rcp85
 input_path=/storage01/rtladerjr/hourly/${FIRSTYEAR}
-output_path=/atlas_scratch/malindgren/WRF_DATA/${FIRSTYEAR}
+output_path=/atlas_scratch/malindgren/WRF_DATA/${GROUPNAME}/${FIRSTYEAR}
 CPSCRIPTNAME=/workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/make_bash_slurm/copy_year_dione_to_atlas_scratch.py
-# ipython ${CPSCRIPTNAME} -- -i $input_path -o $output_path;
-# srun -p main -N 1 -c 32 ipython ${CPSCRIPTNAME} -- -i $input_path -o $output_path;
+ipython ${CPSCRIPTNAME} -- -i $input_path -o $output_path;
 echo "copied:${FIRSTYEAR}"
 wait
 
-for (( year=${FIRSTYEAR}; year<2007; year++ ));
+for (( year=${FIRSTYEAR}; year<2100; year++ ));
     do     
         if [ $year -lt 2100 ]
         then
@@ -18,107 +18,104 @@ for (( year=${FIRSTYEAR}; year<2007; year++ ));
             # # copy year+1 folder to the current directory
             let CPYEAR=$year+1;
             input_path=/storage01/rtladerjr/hourly/${CPYEAR};
-            output_path=/atlas_scratch/malindgren/WRF_DATA/${CPYEAR};
-            # ipython ${CPSCRIPTNAME} -- -i $input_path -o $output_path;
-            echo $input_path;
-            echo $output_path;
-            echo 'copied':"$CPYEAR";
+            output_path=/atlas_scratch/malindgren/WRF_DATA/${GROUPNAME}/${CPYEAR};
+            ipython ${CPSCRIPTNAME} -- -i ${input_path} -o ${output_path};
+            echo ${input_path};
+            echo ${output_path};
+            echo 'copied':${CPYEAR};
         fi
 
         # # move to the proper pre-built .slurm files directory for the given year
-        cd /workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/make_bash_slurm/slurm_scripts/"$year";
-        echo $(pwd);
+        cd /workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/make_bash_slurm/slurm_scripts/${year};
 
-        jid01=$(sbatch ACSNOW_"$year"_gfdl_rcp85.slurm);
+        jid01=$(sbatch ACSNOW_${year}_${GROUPNAME}.slurm);
         jid01=${jid01##* };
-        jid02=$(sbatch ALBEDO_"$year"_gfdl_rcp85.slurm);
+        jid02=$(sbatch ALBEDO_${year}_${GROUPNAME}.slurm);
         jid02=${jid02##* };
-        jid03=$(sbatch CANWAT_"$year"_gfdl_rcp85.slurm);
+        jid03=$(sbatch CANWAT_${year}_${GROUPNAME}.slurm);
         jid03=${jid03##* };
-        # jid04=$(sbatch CLDFRA_"$year"_gfdl_rcp85.slurm);
-        # jid04=${jid04##* };
-        # jid05=$(sbatch CLDFRA_HIGH_"$year"_gfdl_rcp85.slurm);
-        # jid05=${jid05##* };
-        # jid06=$(sbatch CLDFRA_LOW_"$year"_gfdl_rcp85.slurm);
-        # jid06=${jid06##* };
-        # jid07=$(sbatch CLDFRA_MID_"$year"_gfdl_rcp85.slurm);
-        # jid07=${jid07##* };
-        # jid08=$(sbatch GHT_"$year"_gfdl_rcp85.slurm);
-        # jid08=${jid08##* };
-        # jid09=$(sbatch HFX_"$year"_gfdl_rcp85.slurm);
-        # jid09=${jid09##* };
-        # jid10=$(sbatch LH_"$year"_gfdl_rcp85.slurm);
-        # jid10=${jid10##* };
-        # jid11=$(sbatch LWDNB_"$year"_gfdl_rcp85.slurm);
-        # jid11=${jid11##* };
-        # jid12=$(sbatch LWDNBC_"$year"_gfdl_rcp85.slurm);
-        # jid12=${jid12##* };
-        # jid13=$(sbatch LWUPB_"$year"_gfdl_rcp85.slurm);
-        # jid13=${jid13##* };
-        # jid14=$(sbatch LWUPBC_"$year"_gfdl_rcp85.slurm);
-        # jid14=${jid14##* };
-        # jid15=$(sbatch OMEGA_"$year"_gfdl_rcp85.slurm);
-        # jid15=${jid15##* };
-        # jid16=$(sbatch PCPC_"$year"_gfdl_rcp85.slurm);
-        # jid16=${jid16##* };
-        # jid17=$(sbatch PCPNC_"$year"_gfdl_rcp85.slurm);
-        # jid17=${jid17##* };
-        # jid18=$(sbatch PCPT_"$year"_gfdl_rcp85.slurm);
-        # jid18=${jid18##* };
-        # jid19=$(sbatch POTEVP_"$year"_gfdl_rcp85.slurm);
-        # jid19=${jid19##* };
-        # jid20=$(sbatch PSFC_"$year"_gfdl_rcp85.slurm);
-        # jid20=${jid20##* };
-        # jid21=$(sbatch Q2_"$year"_gfdl_rcp85.slurm);
-        # jid21=${jid21##* };
-        # jid22=$(sbatch QBOT_"$year"_gfdl_rcp85.slurm);
-        # jid22=${jid22##* };
-        # jid23=$(sbatch QVAPOR_"$year"_gfdl_rcp85.slurm);
-        # jid23=${jid23##* };
-        # jid24=$(sbatch SEAICE_"$year"_gfdl_rcp85.slurm);
-        # jid24=${jid24##* };
-        # jid25=$(sbatch SH2O_"$year"_gfdl_rcp85.slurm);
-        # jid25=${jid25##* };
-        # jid26=$(sbatch SLP_"$year"_gfdl_rcp85.slurm);
-        # jid26=${jid26##* };
-        # jid27=$(sbatch SMOIS_"$year"_gfdl_rcp85.slurm);
-        # jid27=${jid27##* };
-        # jid28=$(sbatch SNOW_"$year"_gfdl_rcp85.slurm);
-        # jid28=${jid28##* };
-        # jid29=$(sbatch SNOWC_"$year"_gfdl_rcp85.slurm);
-        # jid29=${jid29##* };
-        # jid30=$(sbatch SNOWH_"$year"_gfdl_rcp85.slurm);
-        # jid30=${jid30##* };
-        # jid31=$(sbatch SWDNB_"$year"_gfdl_rcp85.slurm);
-        # jid31=${jid31##* };
-        # jid32=$(sbatch SWDNBC_"$year"_gfdl_rcp85.slurm);
-        # jid32=${jid32##* };
-        # jid33=$(sbatch SWUPB_"$year"_gfdl_rcp85.slurm);
-        # jid33=${jid33##* };
-        # jid34=$(sbatch SWUPBC_"$year"_gfdl_rcp85.slurm);
-        # jid34=${jid34##* };
-        # jid35=$(sbatch T_"$year"_gfdl_rcp85.slurm);
-        # jid35=${jid35##* };
-        # jid36=$(sbatch T2_"$year"_gfdl_rcp85.slurm);
-        # jid36=${jid36##* };
-        # jid37=$(sbatch TBOT_"$year"_gfdl_rcp85.slurm);
-        # jid37=${jid37##* };
-        # jid38=$(sbatch TSK_"$year"_gfdl_rcp85.slurm);
-        # jid38=${jid38##* };
-        # jid39=$(sbatch TSLB_"$year"_gfdl_rcp85.slurm);
-        # jid39=${jid39##* };
-        # jid40=$(sbatch VEGFRA_"$year"_gfdl_rcp85.slurm);
-        # jid40=${jid40##* };
+        jid04=$(sbatch CLDFRA_${year}_${GROUPNAME}.slurm);
+        jid04=${jid04##* };
+        jid05=$(sbatch CLDFRA_HIGH_${year}_${GROUPNAME}.slurm);
+        jid05=${jid05##* };
+        jid06=$(sbatch CLDFRA_LOW_${year}_${GROUPNAME}.slurm);
+        jid06=${jid06##* };
+        jid07=$(sbatch CLDFRA_MID_${year}_${GROUPNAME}.slurm);
+        jid07=${jid07##* };
+        jid08=$(sbatch GHT_${year}_${GROUPNAME}.slurm);
+        jid08=${jid08##* };
+        jid09=$(sbatch HFX_${year}_${GROUPNAME}.slurm);
+        jid09=${jid09##* };
+        jid10=$(sbatch LH_${year}_${GROUPNAME}.slurm);
+        jid10=${jid10##* };
+        jid11=$(sbatch LWDNB_${year}_${GROUPNAME}.slurm);
+        jid11=${jid11##* };
+        jid12=$(sbatch LWDNBC_${year}_${GROUPNAME}.slurm);
+        jid12=${jid12##* };
+        jid13=$(sbatch LWUPB_${year}_${GROUPNAME}.slurm);
+        jid13=${jid13##* };
+        jid14=$(sbatch LWUPBC_${year}_${GROUPNAME}.slurm);
+        jid14=${jid14##* };
+        jid15=$(sbatch OMEGA_${year}_${GROUPNAME}.slurm);
+        jid15=${jid15##* };
+        jid16=$(sbatch PCPC_${year}_${GROUPNAME}.slurm);
+        jid16=${jid16##* };
+        jid17=$(sbatch PCPNC_${year}_${GROUPNAME}.slurm);
+        jid17=${jid17##* };
+        jid18=$(sbatch PCPT_${year}_${GROUPNAME}.slurm);
+        jid18=${jid18##* };
+        jid19=$(sbatch POTEVP_${year}_${GROUPNAME}.slurm);
+        jid19=${jid19##* };
+        jid20=$(sbatch PSFC_${year}_${GROUPNAME}.slurm);
+        jid20=${jid20##* };
+        jid21=$(sbatch Q2_${year}_${GROUPNAME}.slurm);
+        jid21=${jid21##* };
+        jid22=$(sbatch QBOT_${year}_${GROUPNAME}.slurm);
+        jid22=${jid22##* };
+        jid23=$(sbatch QVAPOR_${year}_${GROUPNAME}.slurm);
+        jid23=${jid23##* };
+        jid24=$(sbatch SEAICE_${year}_${GROUPNAME}.slurm);
+        jid24=${jid24##* };
+        jid25=$(sbatch SH2O_${year}_${GROUPNAME}.slurm);
+        jid25=${jid25##* };
+        jid26=$(sbatch SLP_${year}_${GROUPNAME}.slurm);
+        jid26=${jid26##* };
+        jid27=$(sbatch SMOIS_${year}_${GROUPNAME}.slurm);
+        jid27=${jid27##* };
+        jid28=$(sbatch SNOW_${year}_${GROUPNAME}.slurm);
+        jid28=${jid28##* };
+        jid29=$(sbatch SNOWC_${year}_${GROUPNAME}.slurm);
+        jid29=${jid29##* };
+        jid30=$(sbatch SNOWH_${year}_${GROUPNAME}.slurm);
+        jid30=${jid30##* };
+        jid31=$(sbatch SWDNB_${year}_${GROUPNAME}.slurm);
+        jid31=${jid31##* };
+        jid32=$(sbatch SWDNBC_${year}_${GROUPNAME}.slurm);
+        jid32=${jid32##* };
+        jid33=$(sbatch SWUPB_${year}_${GROUPNAME}.slurm);
+        jid33=${jid33##* };
+        jid34=$(sbatch SWUPBC_${year}_${GROUPNAME}.slurm);
+        jid34=${jid34##* };
+        jid35=$(sbatch T_${year}_${GROUPNAME}.slurm);
+        jid35=${jid35##* };
+        jid36=$(sbatch T2_${year}_${GROUPNAME}.slurm);
+        jid36=${jid36##* };
+        jid37=$(sbatch TBOT_${year}_${GROUPNAME}.slurm);
+        jid37=${jid37##* };
+        jid38=$(sbatch TSK_${year}_${GROUPNAME}.slurm);
+        jid38=${jid38##* };
+        jid39=$(sbatch TSLB_${year}_${GROUPNAME}.slurm);
+        jid39=${jid39##* };
+        jid40=$(sbatch VEGFRA_${year}_${GROUPNAME}.slurm);
+        jid40=${jid40##* };
 
-        # jobids="$jid01":"$jid02":"$jid03":"$jid04":"$jid05":"$jid06":"$jid07":"$jid08":"$jid09":"$jid10":"$jid11":"$jid12":"$jid13":"$jid14":"$jid15":"$jid16":"$jid17":"$jid18":"$jid19":"$jid20":"$jid21":"$jid22":"$jid23":"$jid24":"$jid25":"$jid26":"$jid27":"$jid28":"$jid29":"$jid30":"$jid31":"$jid32":"$jid33":"$jid34":"$jid35":"$jid36":"$jid37":"$jid38":"$jid39":"$jid40";
-        jobids="$jid01":"$jid02":"$jid03"
-        # echo $jobids;
+        jobids="$jid01":"$jid02":"$jid03":"$jid04":"$jid05":"$jid06":"$jid07":"$jid08":"$jid09":"$jid10":"$jid11":"$jid12":"$jid13":"$jid14":"$jid15":"$jid16":"$jid17":"$jid18":"$jid19":"$jid20":"$jid21":"$jid22":"$jid23":"$jid24":"$jid25":"$jid26":"$jid27":"$jid28":"$jid29":"$jid30":"$jid31":"$jid32":"$jid33":"$jid34":"$jid35":"$jid36":"$jid37":"$jid38":"$jid39":"$jid40";
 
         # remove the year-1 folder
         SCRIPTNAME=/workspace/UA/malindgren/repos/wrf_utils/snap_wrf_data_prep/make_bash_slurm/remove_dir_atlas_scratch.py;
-        RMDIRNAME=/atlas_scratch/malindgren/WRF_DATA/${year}_TMP;
+        RMDIRNAME=/atlas_scratch/malindgren/WRF_DATA/${year};
         depends=afterok:${jobids};
-        echo ${depends}
 
         srun -n 1 -p main --dependency=$depends ipython $SCRIPTNAME -- -i $RMDIRNAME;
+        echo 'completed removal of directory'
     done;
