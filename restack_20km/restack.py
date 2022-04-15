5cfbe5e21f0d0bd0a44f96cc356521a2e3afef44
Kyle <kmredilla@alaska.edu>
2022-04-15 14:28:41 -0800

    updates to include working code to restack script for performing tasks originally included in improvement step

--- restack_20km/restack.py
+++ restack_20km/restack.py
@@ -7,13 +7,17 @@ Usage:
 """
 
 import argparse
+import datetime
+import importlib.util
 import os
+import sys
 import time
 from multiprocessing import Pool
 from pathlib import Path
 import numpy as np
 import pandas as pd
 import xarray as xr
+from pyproj import Proj, Transformer
 
 
 def interp_1d_along_axis(y):
@@ -21,7 +25,7 @@ def interp_1d_along_axis(y):
     nans = np.isnan(y)
     x = lambda z: z.nonzero()[0]
     y[nans] = np.interp(x(nans), x(~nans), y[~nans])
-    
+
     return y
 
 
@@ -75,9 +79,12 @@ def diff_stacked(stacked_arr):
     """
     height, width = stacked_arr.shape[1:]
     diff_arr = np.concatenate(
-        [np.broadcast_to(np.array([np.nan]), (1, height, width)), np.diff(stacked_arr, axis=0)]
+        [
+            np.broadcast_to(np.array([np.nan]), (1, height, width)),
+            np.diff(stacked_arr, axis=0),
+        ]
     )
-    
+
     return diff_arr
 
 
@@ -101,7 +108,7 @@ def restack_accum(ftimes_df, year, varname, ncpus):
     # which indexes correspond to the year in question?
     ind = [count for count, df in enumerate(groups) if year in df["year"].tolist()]
 
-    # test to be sure they are all chronological -- Should 
+    # test to be sure they are all chronological -- Should
     #  be since dataframe is pre-sorted
     assert np.diff(ind).all() == 1
 
@@ -129,7 +136,7 @@ def restack_accum(ftimes_df, year, varname, ncpus):
     # we need some indexing for slicing the output array to ONLY this current year
     groups_df = pd.concat(groups)  # should be chronological
     (current_year_ind,) = np.where(groups_df["year"] == year)  # along time dimension
-    
+
     # process groups and concatenate 3D cubes along time axis chronologically
     stacked_arrs = [restack(df["filepath"], varname, ncpus) for df in groups]
     arr = np.concatenate([diff_stacked(arr) for arr in stacked_arrs])
@@ -140,7 +147,7 @@ def restack_accum(ftimes_df, year, varname, ncpus):
 
     # make sure we have no leftover negative precip
     arr[arr < 0] = 0
-    
+
     return arr
 
 
@@ -225,6 +232,55 @@ def restack_winds(fps, varname, geogrid_fp, ncpus):
     return stacked_arr
 
 
+def derive_xy(geogrid_fp, wrf_proj_str):
+    """Derive the x and y coordinate axes for the WRF grid
+    
+    Args:
+        geogrid_fp (path_like): path to the WRF geogrid file
+        wrf_proj_str (str): Proj4 string for the WRF grid derived in
+            ancillary/include_latlon.ipynb
+    
+    Returns:
+        tuple of x and y arrays containing x and y coordinate values 
+    """
+    wrf_proj = Proj(wrf_proj_str)
+    wgs_proj = Proj(proj="latlong", datum="WGS84")
+    transformer = Transformer.from_proj(wgs_proj, wrf_proj)
+
+    with xr.open_dataset(geogrid_fp) as geo_ds:
+        # Easting and Northings of the domains center point
+        e, n = transformer.transform(geo_ds.CEN_LON, geo_ds.CEN_LAT)
+        dx, dy = geo_ds.DX, geo_ds.DY
+        nx, ny = geo_ds.dims["west_east"], geo_ds.dims["south_north"]
+
+    # Down left corner of the domain
+    x0 = -(nx - 1) / 2.0 * dx + e
+    y0 = -(ny - 1) / 2.0 * dy + n
+    x = np.arange(nx) * dx + x0
+    # flip the y-axis to match flipped lat/lon grids and flipped data array
+    y = np.flip(np.arange(ny) * dy + y0)
+
+    return x, y
+
+
+def path_import(module_fp):
+    """Import a module given its path. Intended for loading luts.py.
+    
+    Args:
+        module_fp (path_like): path to the module to import
+    
+    Returns:
+        a module object created from the module at path in module_fp
+    """
+    module_name = module_fp.name.split(".")[0]
+    spec = importlib.util.spec_from_file_location(module_name, module_fp)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    return module
+
+
 if __name__ == "__main__":
     # parse some args
     parser = argparse.ArgumentParser(
@@ -248,18 +304,18 @@ if __name__ == "__main__":
         help="path to the .csv file containing parsed filename and forecast times",
     )
     parser.add_argument(
-        "-t",
-        "--template_fp",
-        action="store",
-        dest="template_fp",
-        help="monthly template file that is used for passing global metadata to output NC files.",
-    )
-    parser.add_argument(
         "-o",
         "--out_fp",
         action="store",
         dest="out_fp",
         help="output file path for the new NetCDF hourly data for the input year",
+    )
+    parser.add_argument(
+        "-l",
+        "--luts_fp",
+        action="store",
+        dest="luts_fp",
+        help="Path to luts.py file for the restack_20km pipeline",
     )
     parser.add_argument(
         "-n",
@@ -277,132 +333,127 @@ if __name__ == "__main__":
         default=None,
         help="Path to ancillary WRF geogrid file.",
     )
-    parser.add_argument(
-        "-a",
-        "--accum",
-        action="store_true",
-        dest="accum",
-        default=False,
-        help="Process suppliec variable as an accumulation variable",
-    )
     # parse the args and unpack
     args = parser.parse_args()
     year = args.year
     varname = args.varname
     ftimes_fp = args.ftimes_fp
-    template_fp = args.template_fp
     out_fp = Path(args.out_fp)
+    luts_fp = Path(args.luts_fp)
     ncpus = args.ncpus
     geogrid_fp = args.geogrid_fp
-    accum = args.accum
-    
-    # safety to ensure geogrid filepath, used for wind variables, and accum flag
-    #  for accumulation variables are never both specified
-    assert accum != bool(geogrid_fp)
-    
-    # wrf output standard vars -- [hardwired] for now
-    lon_variable = "g5_lon_1"
-    lat_variable = "g5_lat_0"
+
+    # import the luts table supplied as a path
+    luts = path_import(luts_fp)
 
     # read in pre-built dataframe with forecast_time as a field
     ftimes_df = pd.read_csv(ftimes_fp)
 
-    # run stacking of variable through time and handle winds or accumulation
+    # run the re-stacking of data through time, and handle winds or accumulation
     #  variables as needed
     tic = time.perf_counter()
     # interpolate accumulation vars at `ind`
-    if accum:
+    if varname in luts.accum_varnames:
         arr = restack_accum(ftimes_df, year, varname, ncpus)
     else:
         fps = ftimes_df[ftimes_df["year"] == year]["filepath"]
-        if geogrid_fp:
+        if varname in luts.wind_varnames:
             arr = restack_winds(fps, varname, geogrid_fp, ncpus)
         else:
             arr = restack(fps, varname, ncpus)
-    print(f"Data restacked, time elapsed: {round((time.perf_counter() - tic) / 60, 1)}m")
-
-    # subset the data frame to the desired year -- for naming stuff
-    ftimes_year_df = ftimes_df[(ftimes_df["year"] == year) & (ftimes_df["folder_year"] == year)].reset_index()
-
-    # build the output NetCDF Dataset
-    new_dates = pd.date_range(
-        "-".join(ftimes_year_df.loc[ftimes_year_df.index[0], ["month", "day", "year"]].astype(str)),
-        periods=arr.shape[0],
-        freq="1H",
+    print(
+        f"Data restacked, time elapsed: {round((time.perf_counter() - tic) / 60, 1)}m"
     )
 
-    # get some template data to get some vars from... -- HARDWIRED...
-    mon_tmp_ds = xr.open_dataset(template_fp, decode_times=False)
-    tmp_ds = xr.open_dataset(ftimes_year_df["filepath"].tolist()[0])
-    global_attrs = tmp_ds.attrs.copy()
-    global_attrs["reference_time"] = str(
-        new_dates[0]
-    )  # 1979 hourly does NOT start at day 01...  rather day 02....
-    global_attrs[
-        "proj_parameters"
-    ] = "+proj=stere +lat_0=90 +lat_ts=90 +lon_0=-150 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs"
-    local_attrs = tmp_ds[varname].attrs.copy()
-    xy_attrs = mon_tmp_ds.lon.attrs.copy()
+    # subset the data frame to the desired year -- for naming stuff
+    ftimes_year_df = ftimes_df[
+        (ftimes_df["year"] == year) & (ftimes_df["folder_year"] == year)
+    ].reset_index()
 
-    if len(arr.shape) == 3:
-        # build a new Dataset with the stacked timesteps and
-        #   some we extracted from the input Dataset
-        ds = xr.Dataset(
-            {varname: (["time", "x", "y"], arr.astype(np.float32))},
-            coords={
-                "lon": (["x", "y"], tmp_ds[lon_variable].values),
-                "lat": (["x", "y"], tmp_ds[lat_variable].values),
-                "time": new_dates,
-            },
-            attrs=global_attrs,
-        )
+    # build the output NetCDF Dataset
+    # pull time stamp values from table instead of pandas.date_range()
+    # that would have to be corrected later
+    new_dates = pd.DatetimeIndex(
+        [
+            datetime.datetime(
+                *[row[colname] for colname in ["year", "month", "day", "hour"]]
+            )
+            for i, row in ftimes_year_df.iterrows()
+        ]
+    )
 
-    elif len(arr.shape) == 4:  # (time,levels, x, y )
-        # levelname to use if 4D
-        if varname in ["TSLB", "SMOIS", "SH2O"]:
-            levelname = "lv_DBLY3"
+    tmp_fp = ftimes_year_df["filepath"].iloc[0]
+    with xr.open_dataset(tmp_fp) as tmp_ds:
+        global_attrs = tmp_ds.attrs
+        local_attrs = tmp_ds[varname].attrs
+        encoding = tmp_ds[varname].encoding
+        lon_arr = tmp_ds[luts.lon_variable].values
+        lat_arr = tmp_ds[luts.lat_variable].values
+        # get level name if present, should always be the first one
+        if len(tmp_ds[varname].dims) == 3:
+            levelname = tmp_ds[varname].dims[0]
+            levels = tmp_ds[levelname].values
         else:
-            levelname = "lv_ISBL2"
+            levelname = None
 
-        # build dataset with levels at each timestep
-        sub_ds = xr.open_dataset(ftimes_year_df.iloc[0]["filepath"])
-        ds = xr.Dataset(
-            {varname: (["time", levelname, "x", "y"], arr.astype(np.float32))},
-            coords={
-                "lon": (["x", "y"], tmp_ds[lon_variable].values),
-                "lat": (["x", "y"], tmp_ds[lat_variable].values),
-                "time": new_dates,
-                levelname: sub_ds[levelname],
-            },
-            attrs=global_attrs,
-        )
+    global_attrs.update(luts.global_attrs)
+    global_attrs["reference_time"] = str(new_dates[0])
+
+    x, y = derive_xy(geogrid_fp, global_attrs["proj_parameters"])
+    # build dicts for creating xarray dataset:
+    coords_dict = {
+        # flip the lon and lat arrays to provide more intuitive orientation
+        #  within dataset
+        "time": (["time"], new_dates, luts.coord_attrs["time"]),
+        # "xc" and "yc" naming retained from original restacking pipeline
+        "yc": (["yc"], y, luts.coord_attrs["yc"]),
+        "xc": (["xc"], x, luts.coord_attrs["xc"]),
+        "lon": (["yc", "xc"], np.flipud(lon_arr), luts.coord_attrs["lon"]),
+        "lat": (["yc", "xc"], np.flipud(lat_arr), luts.coord_attrs["lat"]),
+    }
+
+    # handle variables with a 4th dim level
+    if levelname is not None:
+        new_levelname = luts.levelnames[levelname]
+        dims = ["time", new_levelname, "yc", "xc"]
+        coords_dict[new_levelname] = [
+            [new_levelname],
+            levels,
+            luts.var_attrs[levelname],
+        ]
     else:
-        raise BaseException(
-            "incorrect number of dimensions in arr. Must be 3 or 4 (as currently implemented)"
-        )
+        dims = ["time", "yc", "xc"]
 
-    # set the local attrs for the given variable we are stacking
-    ds[varname].attrs = local_attrs
-
-    # set the lon/lat vars attrs with the existing attrs from the monthly dataset now...
-    ds[varname].attrs = xy_attrs
+    crs_varname = "polar_stereographic"
+    local_attrs["grid_mapping"] = crs_varname
+    wrf_proj = Proj(global_attrs["proj_parameters"])
+    new_varname = varname.lower()
+    data_dict = {
+        # add the data flipped along y axis to match flipped lat/lon arrays
+        #  and y-coordinate array
+        new_varname: (dims, np.flip(arr.astype(np.float32), axis=1), local_attrs),
+        # create CRS variable and add info
+        crs_varname: ([], np.array(b""), wrf_proj.crs.to_cf()),
+    }
+    ds = xr.Dataset(data_dict, coords_dict, global_attrs)
 
     # set output compression and encoding for serialization
-    encoding = ds[varname].encoding
     encoding.update(
         zlib=True, complevel=5, contiguous=False, chunksizes=None, dtype="float32"
     )
-    ds[varname].encoding = encoding
+    ds[new_varname].encoding = encoding
 
     # write to disk
-    # remove an existing one since I think that is best practice here (<- original author, untested but leaving for now)
+    # remove an existing one since I think that is best practice
+    #   here (<- original author, untested but leaving for now)
     if out_fp.exists():
         out_fp.unlink()
-    
+
     tic = time.perf_counter()
     ds.to_netcdf(out_fp)
-
-    print((
-        f"Restacked data for {varname}, {year} written to {out_fp} at {time.ctime()}, "
-        f"time elapsed: {round((time.perf_counter() - tic) / 60, 1)}m"
-    ))
+    print(
+        (
+            f"Restacked data for {varname}, {year} written to {out_fp} at {time.ctime()}, "
+            f"time elapsed: {round((time.perf_counter() - tic) / 60, 1)}m"
+        )
+    )
