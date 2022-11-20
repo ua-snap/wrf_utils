67ea81cee5247b587c5abd7a6eac606d48c16be3
Kyle <kmredilla@alaska.edu>
2022-11-20 12:33:37 -0900

    fix check for __main__ in __name__

--- restack_20km/stage_hourly.py
+++ restack_20km/stage_hourly.py
@@ -1,6 +1,6 @@
 """Stage hourly WRF outputs for WRF group set in WRF_GROUP env var. 
 
-Instead of doing a single batch_stage for the entire WRF group directory, this script will iterate over the years 
+Instead of doing a single batch_stage for the entire WRF group directory, this script will iterate over all years available for a single WRF group and stage the yearly directories.
 """
 
 import time
@@ -10,7 +10,7 @@ from config import *
 import luts
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     years = luts.groups[group]["years"]
     wrf_dir = luts.groups[group]["directory"]
 
